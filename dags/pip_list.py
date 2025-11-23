e8ed1161191d3353627024d37bed616870620862
Владислав <56272445+frankO1iver@users.noreply.github.com>
2025-11-23 20:24:17 +0300

    Create pip_list.py

---
+++ dags/pip_list.py
@@ -0,0 +1,19 @@
+from airflow import DAG
+# В Airflow 3 BashOperator переехал в стандартный провайдер
+from airflow.providers.standard.operators.bash import BashOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2025, 11, 1),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+    show_packages = BashOperator(
+        task_id="show_packages",
+        bash_command="pip3 list"
+    )
